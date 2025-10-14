ebc702506c30b6eb2580d0fd2d9f3cf291cc46fc
Esa <esa-pitkanen@hotmail.com>
2025-10-14 12:50:18 +0300

    refactor(ship): convert isSunk property to getter method

--- src/classes/ships.js
+++ src/classes/ships.js
@@ -2,6 +2,13 @@ export class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
-    this.isSunk = false;
+  }
+
+  hit() {
+    this.hits++
+  }
+
+  get isSunk() {
+    return this.length <= this.hits;
   }
 }
