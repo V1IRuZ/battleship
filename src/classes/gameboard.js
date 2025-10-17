6b8c7aa9783f9ded4637f1ecee0293107b17fac3
Esa <esa-pitkanen@hotmail.com>
2025-10-17 12:56:58 +0300

    feat(gameboard): initialize ship instances in the ships property

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,6 +1,15 @@
+import { Ship } from "./ships.js";
+
 class Gameboard {
   constructor() {
     this.board = Array.from({ length: 10 }, () => Array(10).fill(null));
+    this.ships = [
+      new Ship("carrier", 5),
+      new Ship("battleship", 4),
+      new Ship("cruiser1", 3),
+      new Ship("cruiser2", 3),
+      new Ship("destroyer", 2),
+      new Ship("submarine", 1),
+    ];
   }
 }
-
