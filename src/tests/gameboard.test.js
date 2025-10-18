556231962bf89b7a9fbffe832edbbfb5014b17e1
Esa <esa-pitkanen@hotmail.com>
2025-10-18 11:47:10 +0300

    test(gameboard): add test for placing ship across multiple cells

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -11,5 +11,15 @@ describe("Gameboard class", () => {
       gameBoard.placeShip(0, [3, 3]);
       expect(gameBoard.board[3][3]).toBe("carrier");
     });
+
+    test.each([
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+    ])("fills consecutive cells based on ship length (%p, %p)", (x, y) => {
+      gameBoard.placeShip(1, [1, 1]);
+      expect(gameBoard.board[x][y]).toBe("battleship");
+    });
   });
 });
