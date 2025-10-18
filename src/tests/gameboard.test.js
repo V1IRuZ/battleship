406edf5497a9e7d15f6caa4aaa1021182bb8b9c7
Esa <esa-pitkanen@hotmail.com>
2025-10-18 14:11:20 +0300

    test(gameboard): update placeShip tests to include rotation parameter

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -8,7 +8,7 @@ describe("Gameboard class", () => {
 
   describe("placeShip method", () => {
     test("places ship on gameboard", () => {
-      gameBoard.placeShip(0, [3, 3]);
+      gameBoard.placeShip(0, [3, 3], "horizontal");
       expect(gameBoard.board[3][3]).toBe("carrier");
     });
 
@@ -18,13 +18,22 @@ describe("Gameboard class", () => {
       [1, 3],
       [1, 4],
     ])("fills consecutive cells based on ship length (%p, %p)", (x, y) => {
-      gameBoard.placeShip(1, [1, 1]);
+      gameBoard.placeShip(1, [1, 1], "horizontal");
       expect(gameBoard.board[x][y]).toBe("battleship");
     });
 
     test("only fills the cells marked for it", () => {
-      gameBoard.placeShip(1, [1, 1]);
+      gameBoard.placeShip(1, [1, 1], "horizontal");
       expect(gameBoard.board[1][5]).toBe(null);
+    });
+
+    test.each([
+      [2, 5],
+      [3, 5],
+      [4, 5],
+    ])("fills cells vertically as well (%p, %p)", (x, y) => {
+      gameBoard.placeShip(2, [2, 5], "vertical");
+      expect(gameBoard.board[x][y]).toBe("cruiser1");
     });
   });
 });
