735478db7283c36b63acd01d9244a077dc715879
Esa <esa-pitkanen@hotmail.com>
2025-10-22 17:24:03 +0300

    wip(player): add a temporary method to place ships on the board

--- src/classes/player.js
+++ src/classes/player.js
@@ -4,8 +4,19 @@ class Player {
   constructor() {
     this.gameBoard = new Gameboard();
   }
+
+  defaultPlacement() {
+    this.gameBoard.placeShip(0, [1, 1], "horizontal");
+    this.gameBoard.placeShip(1, [3, 0], "vertical");
+    this.gameBoard.placeShip(2, [3, 2], "vertical");
+    this.gameBoard.placeShip(3, [3, 4], "vertical");
+    this.gameBoard.placeShip(4, [7, 2], "horizontal");
+    this.gameBoard.placeShip(5, [8, 8], "vertical");
+  }
 }
 
 class RealPlayer extends Player {}
 
 class ComputerPlayer extends Player {}
+
+export { Player };
