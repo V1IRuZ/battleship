063102a2f8d1f2d73f7e91d76caff0489eda7ec7
Esa <esa-pitkanen@hotmail.com>
2025-10-23 18:37:50 +0300

    feat(controller): refactor the init method and add an id argument for players

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -4,33 +4,25 @@ import { events } from "../ui/events.js";
 
 export class GameController {
   constructor() {
-    this.player1 = new Player();
-    this.player2 = new Player();
+    this.player1 = new Player("player1");
+    this.player2 = new Player("player2");
 
     this.init();
   }
 
   init() {
     const content = document.querySelector("#content");
-
-    const player1Board = document.createElement("div");
-    player1Board.classList.add("board");
     this.player1.defaultPlacement();
-    render.drawBoard(player1Board, this.player1.gameBoard.board);
-    content.appendChild(player1Board);
-
-    const player2Board = document.createElement("div");
-    player2Board.classList.add("board");
-    player2Board.classList.add("opponent");
     this.player2.defaultPlacement();
-    render.drawBoard(player2Board, this.player2.gameBoard.board);
-    content.appendChild(player2Board);
+    
+    render.showBoards(this.player1, content);
+    render.showBoards(this.player2, content);
 
-    events.bindBoardClicks((x, y) => this.handleBoardClicks(x, y));
+    events.bindBoardClicks((x, y) => this.handleBoardClicks(x, y, this.player2));
   }
 
-  handleBoardClicks(x, y) {
-    this.player2.gameBoard.receiveAttack(x, y);
-    render.showHitandMiss(x, y);
+  handleBoardClicks(x, y, playerObj) {
+    playerObj.gameBoard.receiveAttack(x, y);
+    render.showHitandMiss(x, y, playerObj.id);
   }
 }
