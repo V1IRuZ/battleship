7a1336e6ed0f402f759eca792306545601e790b3
Esa <esa-pitkanen@hotmail.com>
2025-10-25 09:49:18 +0300

    refactor(events, controller): update event binding and parameter handling
    - Adjusted events.js methods to accept new parameter structure
    - Updated gameController to bind UI events to controller methods accordingly
    - Method names changed

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -1,28 +1,41 @@
-import { Player } from "../classes/player.js";
+import { ComputerPlayer, Player } from "../classes/player.js";
 import { render } from "../ui/render.js";
 import { events } from "../ui/events.js";
 
 export class GameController {
   constructor() {
     this.player1 = new Player("player1");
-    this.player2 = new Player("player2");
+    this.player2 = new ComputerPlayer("player2");
 
-    this.init();
+    this.initSinglePlayer();
   }
 
-  init() {
+  initSinglePlayer() {
     const content = document.querySelector("#content");
     this.player1.defaultPlacement();
     this.player2.defaultPlacement();
-    
+
     render.showBoards(this.player1, content);
     render.showBoards(this.player2, content);
-
-    events.bindBoardClicks((x, y) => this.handleBoardClicks(x, y, this.player2));
+    
+    const board = document.querySelector(".player2");
+    events.bindBoardClicks(board, (x, y) => {
+      this.handleSinglePlayerClicks(x, y)
+    });
   }
 
-  handleBoardClicks(x, y, playerObj) {
-    playerObj.gameBoard.receiveAttack(x, y);
-    render.showHitandMiss(x, y, playerObj.id);
+  handleSinglePlayerClicks(x, y) {
+    try {
+      this.player2.gameBoard.receiveAttack(x, y);
+      render.showHitandMiss(x, y, this.player2.id);
+      
+      const randomX = Math.floor(Math.random() * 10);
+      const randomY = Math.floor(Math.random() * 10);
+      
+      this.player1.gameBoard.receiveAttack(randomX, randomY);
+      render.showHitandMiss(randomX, randomY, this.player1.id);
+    } catch(err) {
+      console.log(err);
+    }
   }
 }
--- src/ui/events.js
+++ src/ui/events.js
@@ -1,12 +1,11 @@
 export const events = {
-  bindBoardClicks(callback, playerObj) {
-    const board = document.querySelector(".player2");
+  bindBoardClicks(board, callback) {
     board.addEventListener("click", (e) => {
       if (!e.target.classList.contains("cell")) return;
       const x = Number(e.target.dataset.x);
       const y = Number(e.target.dataset.y);
 
-      callback(x, y, playerObj);
+      callback(x, y);
     });
   },
 };
