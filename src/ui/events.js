1e352eb70a8134cb0c7efddfa20332df6a00094a
Esa <esa-pitkanen@hotmail.com>
2025-10-25 13:01:47 +0300

    feat(events): add validations for the coordinates used

--- src/ui/events.js
+++ src/ui/events.js
@@ -2,6 +2,9 @@ export const events = {
   bindBoardClicks(board, callback) {
     board.addEventListener("click", (e) => {
       if (!e.target.classList.contains("cell")) return;
+      if (e.target.classList.contains("miss")) return;
+      if (e.target.classList.contains("hit")) return;
+      
       const x = Number(e.target.dataset.x);
       const y = Number(e.target.dataset.y);
 
