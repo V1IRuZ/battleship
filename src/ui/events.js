86ef6670fd9c0f0d84b75afb924d74ecb69fe795
Esa <esa-pitkanen@hotmail.com>
2025-11-08 12:40:58 +0200

    feat(events): add method binding an event listener to the menu button

--- src/ui/events.js
+++ src/ui/events.js
@@ -4,11 +4,18 @@ export const events = {
       if (!e.target.classList.contains("cell")) return;
       if (e.target.classList.contains("miss")) return;
       if (e.target.classList.contains("hit")) return;
-      
+
       const x = Number(e.target.dataset.x);
       const y = Number(e.target.dataset.y);
 
       callback(x, y);
+    });
+  },
+  bindSinglePlayerClick(board, callback) {
+    board.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("single-player")) return;
+      
+      callback();
     });
   },
 };
