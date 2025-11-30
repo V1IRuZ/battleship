df6e0337e3cb974f733eba6d2f0e4a1681da7cf6
Esa <esa-pitkanen@hotmail.com>
2025-11-30 13:50:23 +0200

    refactor(player): remove unnecessary defaultPlacement method

--- src/classes/player.js
+++ src/classes/player.js
@@ -6,17 +6,6 @@ class Player {
     this.id = id;
     this.gameBoard = new Gameboard();
   }
-
-
-
-  defaultPlacement() {
-    this.gameBoard.placeShip(0, [1, 1], "horizontal");
-    this.gameBoard.placeShip(1, [3, 0], "vertical");
-    this.gameBoard.placeShip(2, [3, 2], "vertical");
-    this.gameBoard.placeShip(3, [3, 4], "vertical");
-    this.gameBoard.placeShip(4, [7, 2], "horizontal");
-    this.gameBoard.placeShip(5, [8, 8], "vertical");
-  }
 }
 
 class RealPlayer extends Player {
